3302412fad12acf2b4fbc0d701033e55bac5112b
Natsuki Ikeguchi <me@s6n.jp>
2024-03-20 14:52:25 +0900

    feat: Initial commit

---
+++ App.tsx
@@ -0,0 +1,66 @@
+import { initializeApp } from "firebase/app";
+import { OAuthProvider, type User, getAuth, signInWithRedirect } from "firebase/auth";
+import { type FC, useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+});
+
+const firebaseAuth = getAuth(firebaseApp);
+
+export const App: FC = () => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    return firebaseAuth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
+  const handleLogin = async () => {
+    await signInWithRedirect(firebaseAuth, new OAuthProvider("oidc.slack"));
+  };
+
+  const handleLogout = async () => {
+    await firebaseAuth.signOut();
+  };
+
+  if (!currentUser) {
+    return (
+      <div>
+        <p>Not Logged In</p>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>Logged In</p>
+      {currentUser.photoURL && <img alt="" width={128} height={128} src={currentUser.photoURL} />}
+      <dl>
+        <dt>ID</dt>
+        <dd>{currentUser.uid}</dd>
+        <dt>Name</dt>
+        <dd>{currentUser.displayName}</dd>
+        <dt>Email Address</dt>
+        <dd>{currentUser.email}</dd>
+        <dt>{currentUser.phoneNumber}</dt>
+      </dl>
+      <button type="button" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
+};
+
+// biome-ignore lint/style/noNonNullAssertion:
+createRoot(document.getElementById("root")!).render(<App />);
